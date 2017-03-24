216c45b47f7a55f94881da93882c8c7ca7824b55
brunoba <brunobarros2014@gmail.com>
2017-03-24 00:02:53 -0300

    Ajustes no metodo de colisao

--- DilMandioca.java
+++ DilMandioca.java
@@ -16,16 +16,16 @@ public class DilMandioca extends AdvancedRobot {
 		setScanColor(new Color(0, 255, 0));
 
 		// Loop forever
-		while (true) {
-			setAhead(40000);
-			movingForward = true;
-			setTurnRight(90);
-			waitFor(new TurnCompleteCondition(this));
-			setTurnLeft(180);
-			waitFor(new TurnCompleteCondition(this));
-			setTurnRight(180);
-			waitFor(new TurnCompleteCondition(this));
-		}
+//		while (true) {
+//			setAhead(40000);
+//			movingForward = true;
+//			setTurnRight(90);
+//			waitFor(new TurnCompleteCondition(this));
+//			setTurnLeft(180);
+//			waitFor(new TurnCompleteCondition(this));
+//			setTurnRight(180);
+//			waitFor(new TurnCompleteCondition(this));
+//		}
 	}
 
 	public void onHitWall(HitWallEvent e) {
@@ -45,35 +45,36 @@ public class DilMandioca extends AdvancedRobot {
 
 	public void onScannedRobot(ScannedRobotEvent e) {
 		
-		System.out.println(e.getDistance());
-		
-		if(e.getDistance() >= 0.0  && e.getDistance() <= 100.00 ){
-			System.out.println("Tiro com potencia 5");
-			fire(5);
-		}
-		else if(e.getDistance() > 100.0  && e.getDistance() <= 200.00 ){
-			System.out.println("Tiro com potencia 4");
-			fire(4);
-		}
-		else if(e.getDistance() > 200.0  && e.getDistance() <= 300.00 ){
-			System.out.println("Tiro com potencia 3");
-			fire(3);
-		}
-		else if(e.getDistance() > 300.0  && e.getDistance() <= 400.00 ){
-			System.out.println("Tiro com potencia 2");
-			fire(2);
-		}
-		else if(e.getDistance() > 500.0  && e.getDistance() <= 600.00 ){
-			System.out.println("Tiro com potencia 1");
-			fire(1);
-		}
-		
-		System.out.println("_______________");
+//		System.out.println(e.getDistance());
+//		
+//		if(e.getDistance() >= 0.0  && e.getDistance() <= 100.00 ){
+//			System.out.println("Tiro com potencia 5");
+//			fire(5);
+//		}
+//		else if(e.getDistance() > 100.0  && e.getDistance() <= 200.00 ){
+//			System.out.println("Tiro com potencia 4");
+//			fire(4);
+//		}
+//		else if(e.getDistance() > 200.0  && e.getDistance() <= 300.00 ){
+//			System.out.println("Tiro com potencia 3");
+//			fire(3);
+//		}
+//		else if(e.getDistance() > 300.0  && e.getDistance() <= 400.00 ){
+//			System.out.println("Tiro com potencia 2");
+//			fire(2);
+//		}
+//		else if(e.getDistance() > 500.0  && e.getDistance() <= 600.00 ){
+//			System.out.println("Tiro com potencia 1");
+//			fire(1);
+//		}
+//		
+//		System.out.println("_______________");
 	}
 
 	public void onHitRobot(HitRobotEvent e) {
 		
-		//e.get
+		System.out.println(e.getBearing());
+			
 	
 	}
 }
