4f44d556e3cb0f856d1f0fc2088d5389de6c93ad
brunoba <brunobarros2014@gmail.com>
2017-03-26 23:21:13 -0300

    Ajuste no Target

--- DilMandioca.java
+++ DilMandioca.java
@@ -16,6 +16,9 @@ public class DilMandioca extends AdvancedRobot {
 		setScanColor(new Color(0, 255, 0));
 		
 		while (true) {
+			
+			System.out.println("Daale");
+			
 			setAhead(40000);
 			movingForward = true;
 			setTurnRight(90);
@@ -29,6 +32,7 @@ public class DilMandioca extends AdvancedRobot {
 
 	public void onHitWall(HitWallEvent e) {
 		// Bounce off!
+		System.out.println("Merda, bati na parede");
 		reverseDirection();
 	}
 
@@ -44,7 +48,15 @@ public class DilMandioca extends AdvancedRobot {
  
 	public void onScannedRobot(ScannedRobotEvent e) {
 		
-		System.out.println(e.getDistance());
+
+		//para mirar o radar no adversário.
+		turnRadarRight(anguloRelativo(e.getBearing()+getHeading()-getRadarHeading()));
+		
+		//para mirar o canhão no adversário.
+		turnGunRight(anguloRelativo(e.getBearing()+getHeading()-getGunHeading())); 
+		
+		//para virar seu robô em direção do adversário
+		turnRight(anguloRelativo(e.getBearing())); 
 		
 		if(e.getDistance() >= 0.0  && e.getDistance() <= 50.00 ){
 			fire(3);
@@ -57,10 +69,52 @@ public class DilMandioca extends AdvancedRobot {
 		}
 	}
 	
-	public void onHitRobot(HitRobotEvent e) {		
+	public void onHitRobot(HitRobotEvent e) {	
+		
+		//para mirar o radar no adversáriåo.
+		turnRadarRight(anguloRelativo(e.getBearing()+getHeading()-getRadarHeading()));
+		
+		//para mirar o canhão no adversário.
+		turnGunRight(anguloRelativo(e.getBearing()+getHeading()-getGunHeading())); 
 
-		System.out.println(e.getBearing());
+//		if(e.getBearing() >= 0){
+//			if(getGunHeading() >= 0 && getGunHeading() < 180){
+//				
+//			}
+//			System.out.println("Direita");
+//		}
+//		else{
+//			if(getGunHeading() >= 0 && getGunHeading() < 180){
+//				
+//			}
+//			System.out.println("Esquerda");
+//		}
+			
 			
 	}
 	
+	public void onRobotDeath(RobotDeathEvent e){
+		System.out.println(e.getName()+" aquele abraço!");
+	}
+	
+	public void onWin(){
+		for (int i = 0; i < 50; i++) {
+			turnRight(30);
+			turnLeft(30);
+		}
+	}
+	
+	public double anguloRelativo(double ANG) {
+		if (ANG> -180 && ANG<= 180) {
+			return ANG;
+		}
+		double REL = ANG;
+		while (REL<= -180) {
+			REL += 360;
+		}
+		while (ANG> 180) {
+			REL -= 360;
+		}
+		return REL;
+	}
 }
